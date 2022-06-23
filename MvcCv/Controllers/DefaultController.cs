b92ad4726f668a545c91b261d019737c3be92fd9
nusret <yalcnusret@gmail.com>
2022-06-23 11:04:41 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var values = db.Abouts.ToList();
+            return View(values);
+        }
+        public PartialViewResult Experiences()
+        {
+            var experience = db.Experiences.ToList();
+            return PartialView(experience);
+        }
+    }
+}
