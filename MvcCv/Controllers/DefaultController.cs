2b0d0dcf7058f3a08523abe567d8af6d1a770101
nusret <yalcnusret@gmail.com>
2022-06-23 16:50:45 +0300

    Admin added

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -21,5 +21,44 @@ namespace MvcCv.Controllers
             var experience = db.Experiences.ToList();
             return PartialView(experience);
         }
+
+        public PartialViewResult Educations()
+        {
+            var education = db.Educations.ToList();
+            return PartialView(education);
+        }
+
+        public PartialViewResult Skills()
+        {
+            var skill = db.Skills.ToList();
+            return PartialView(skill);
+        }
+
+        public PartialViewResult Hobbies()
+        {
+            var hobby = db.Hobbies.ToList();
+            return PartialView(hobby);
+        }
+
+        public PartialViewResult Awards()
+        {
+            var award = db.Awards.ToList();
+            return PartialView(award);
+        }
+
+        [HttpGet]
+        public PartialViewResult Contacts()
+        {
+            var contact = db.Contacts.ToList();
+            return PartialView(contact);
+        }
+        [HttpPost]
+        public PartialViewResult Contacts(Contacts t)
+        {
+            t.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Contacts.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
     }
 }
