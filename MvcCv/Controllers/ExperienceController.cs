e738f3817adca4dcf03dffc6444a042debdbd717
nusret <yalcnusret@gmail.com>
2022-06-24 13:32:25 +0300

    Added repositories

---
+++ MvcCv/Controllers/ExperienceController.cs
@@ -0,0 +1,28 @@
+﻿using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class ExperienceController : Controller
+    {
+       //get: experience
+        ExperienceRepository _experienceRepository;
+
+        public ExperienceController(ExperienceRepository experienceRepository)
+        {
+            _experienceRepository = experienceRepository;
+        }
+
+        // ExperienceRepository repository = new ExperienceRepository();
+
+        public ActionResult Index()
+        {
+            
+            return View(_experienceRepository.GetAll());
+        }
+    }
+}
---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    public class GenericRepository<T> where T : class,new()
+    {
+        DbCvEntities db =new DbCvEntities();
+
+        public List<T> GetAll()
+        {
+            //using (DbCvEntities db = new DbCvEntities())
+            //{
+                return db.Set<T>().ToList();
+           // }    
+        }
+
+        public void Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+
+            db.Set<T>().Remove(entity);
+            db.SaveChanges();
+        }
+
+        public T Get (int id)
+        {
+            return db.Set<T>().Find(id);
+
+        }
+        public void Update(T entity)
+        {
+            db.SaveChanges();
+        }
+
+
+    }
+}
